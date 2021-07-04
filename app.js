87f42f4518a4a6e14ccd554c0db9abbf997e5e78
bohecola <bohecola@outlook.com>
2021-07-04 21:20:23 +0800

    install mysql2

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const expressHbs = require('express-handlebars') // 引入express-handlebars
 
-const errorController = require('./controllers/error')
+const errorController = require('./controllers/error');
+const db = require('./utils/database');
 
 const app = express();
 
 // handlebars 可以简写 hbs, 并配置好相应设置
 // app.engine('hbs', expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'})); // 设置模板引擎handlebars 并初始化引擎expressHbs()
-app.set('view engine', 'ejs')
-// app.set('view engine', 'hbs');
-// app.set('view engine', 'pug'); // 设置模板引擎
+app.set('view engine', 'ejs') // 设置模板引擎
 app.set('views', 'views'); // 不设置默认值是 process.cwd() + 'views' 当前文件夹下的views
 
 const adminRoutes = require('./routes/admin');
