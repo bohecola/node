67bf6307fb90bc5d2af34f28ddf3e6079c7fa182
bohecola <bohecola@outlook.com>
2021-07-05 16:16:08 +0800

    添加订单模型

--- controllers/shop.js
+++ controllers/shop.js
@@ -100,23 +100,45 @@ exports.postCartDeleteProduct = (req, res, next) => {
     })
     .then(result => {
       res.redirect('/cart');
-    }).catch(err => console.log(err));
-  // Product.findById(prodId, product => {
-  //   Cart.deleteProduct(prodId, product.price);
-  //    res.redirect('/cart');
-  // })
+    })
+    .catch(err => console.log(err));
+}
+
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  req.user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts();
+    })
+    .then(products => {
+      return req.user
+        .createOrder().then(order => {
+          order.addProducts(products.map(product => {
+            product.orderItem = { quantity: product.cartItem.quantity }
+            return product;
+          }))
+        }).catch(err => console.log(err));
+    })
+    .then(result => {
+      fetchedCart.setProducts(null);
+    })
+    .then(result => {
+      res.redirect('/orders')
+    })
+    .catch(err => console.log(err))
 }
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: '我的订单'
-  }) 
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: '结算页面'
-  }) 
+  req.user
+    .getOrders({include: ['products']})
+    .then(orders => {
+      console.log(orders);
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: '我的订单',
+        orders: orders
+      }) 
+    }).catch(err => console.log(err));
 };
